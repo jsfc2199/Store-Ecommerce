5ca98a1e5ac7ee99a126ac9a129233e08e4d55e2
Juan Sebastian <jsfc2199@gmail.com>
2022-08-27 12:39:57 -0500

    Project started from here

---
+++ src/main/java/com/jsfc/store/store/JsfcStoreApplication.java
@@ -0,0 +1,13 @@
+package com.jsfc.store.store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsfcStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JsfcStoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jsfc/store/store/JsfcStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jsfc.store.store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JsfcStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
